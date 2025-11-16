4f1b09545693766e573dd88411fc08d29791d337
Nicolas Adamczyk <nicolas.adamczyk.pro@gmail.com>
2025-11-16 12:50:45 +0100

    Commit 16/11

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,5 @@ import svgr from 'vite-plugin-svgr'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
-  // base: '/iss-tracker',
-  // build: {
-  //   outDir: 'docs',
-  // },
+  base: '/iss-tracker/',
 })
